41321a8bec92e5c3a3e24743a32cbd958cfbf37a
Sharjun Hussain <sharjoonibnuhussain99@gmail.com>
2025-03-21 15:27:20 +0530

    fix:navbar items changes and fixes

--- app/[locale]/Components/CurrencyLangModel.jsx
+++ app/[locale]/Components/CurrencyLangModel.jsx
@@ -54,7 +54,7 @@ const CurrencyLanguageDialog = () => {
           </DialogTitle>
         </DialogHeader>
 
-        <div className="relative mt-2">
+        {/* <div className="relative mt-2">
           <Search className="absolute left-3 top-3 h-4 w-4 text-gray-400" />
           <Input
             className="pl-10 pr-4 py-2 rounded-full"
@@ -62,7 +62,7 @@ const CurrencyLanguageDialog = () => {
             value={searchQuery}
             onChange={(e) => setSearchQuery(e.target.value)}
           />
-        </div>
+        </div> */}
 
         <Tabs defaultValue="language" className="mt-4">
           <TabsList className="w-full">
